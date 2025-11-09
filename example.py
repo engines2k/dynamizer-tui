067b49fdf56a14e3b94f7183cd397dfcf1424fc1
engines2k <syncorex0@gmail.com>
2025-11-08 21:56:02 -0700

    sandbox output of fft

--- example.py
+++ example.py
@@ -1,9 +1,34 @@
-import librosa
+import numpy as np
+import sounddevice as sd
 
-filename = librosa.example('nutcracker')
+duration = 5.5
 
-y, sr = librosa.load(filename)
+outputs = sd.query_devices()
 
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+print("printing outputs...")
+print(outputs)
 
-print(f"Estimated tempo: {tempo} beats per minute")
+my_samplerate = 44100
+my_d = 1 / my_samplerate
+
+def callback(indata, outdata, frames, time, status):
+    for channel in indata:
+        channel[0]
+    if status:
+        print(status)
+    outdata[:] = indata
+
+    channel = indata[:, 0]
+    fourier = np.fft.fft(channel)
+    #n = channel.size
+    #freq = np.fft.fftfreq(n, d=my_d)
+
+    # print the value in bin 3 [88hz(?)]
+    eight_eight_hz = fourier.real[2]
+    if eight_eight_hz > .5:
+        print("{:>10.3f}".format(eight_eight_hz))
+
+
+with sd.Stream(channels=2, blocksize=1000, callback=callback) as stream:
+    print(f"Reading from input {stream.device}, with a sample rate of {stream.samplerate} and size of {stream.samplesize}")
+    sd.sleep(int(duration * 1000))
