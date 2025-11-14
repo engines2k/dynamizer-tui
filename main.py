e9d9c546753992a5696cf9746b9f26e5fd22def4
engines2k <syncorex0@gmail.com>
2025-11-13 22:50:09 -0700

    ORIENTED OBJECTS

--- main.py
+++ main.py
@@ -3,49 +3,77 @@ import numpy as np
 import time
 from itertools import chain
 
-my_samplerate = 44100
-my_d = 1 / my_samplerate
+# lol
+LEFT_MONITOR = "Scarlett 2i2 3rd Gen Headphones / Line 1-2:monitor_FL"
+RIGHT_MONITOR = "Scarlett 2i2 3rd Gen Headphones / Line 1-2:monitor_FR"
 
-client = jack.Client("Visualizer")
+class AudioConnector():
+    def __init__(self, process) -> None:
+        self.client = jack.Client("Visualizer")
+        self.client.inports.register("left")
+        self.client.inports.register("right")
+        self.client.set_process_callback(process)
+        self.client.set_shutdown_callback(self.shutdown)
 
-client.inports.register("left")
-client.inports.register("right")
+    def shutdown(self, status, reason):
+        print("JACK shutdown:", status, reason)
 
-@client.set_process_callback
-def process(frames):
+    def activate(self):
+        self.client.activate()
 
-    in_data = client.inports[0].get_array() # type: ignore
+    def deactivate(self):
+        self.client.deactivate()
 
-    fourier = np.fft.fft(in_data)
+    def connect_devices(self):
+        self.client.connect(LEFT_MONITOR, "Visualizer:left")
+        self.client.connect(RIGHT_MONITOR, "Visualizer:right")
 
+    @property
+    def inports(self):
+        return self.client.inports
 
-    res = ""
+class Dynamizer():
+    sample_rate = 44100
+    sample_d = 1 / sample_rate
 
-    for i in chain(range(1, 6, 2),  range(7, len(fourier.real-50), 13)):
-        mag = np.abs(fourier.real[i])  # bin near ~88 Hz
-        if i < 5:
-            res += " "
-        if mag > 3/i*18:
-            res += f" {mag:1.0f} "
-        else:
-            res += " . "
-            
+    def __init__(self):
+        self.audio_connector = AudioConnector(self.process)
 
-    print(res)
+    def activate(self):
+        self.audio_connector.activate()
+        self.audio_connector.connect_devices()
 
-@client.set_shutdown_callback
-def shutdown(status, reason):
-    print("JACK shutdown:", status, reason)
+    def process(self, frames):
+        inports = self.audio_connector.inports
+        # Get frequency bins
+        in_data = inports[0].get_array() # type: ignore
+        fourier = np.fft.fft(in_data)
 
-with client:
-    client.connect("Scarlett 2i2 3rd Gen Headphones / Line 1-2:monitor_FL", "Visualizer:left")
-    client.connect("Scarlett 2i2 3rd Gen Headphones / Line 1-2:monitor_FR", "Visualizer:right")
+        res = ""
+        for i in chain(range(1, 6, 2),  range(7, len(fourier.real-50), 13)):
+            mag = np.abs(fourier.real[i])  # bin near ~88 Hz
+            if i < 5:
+                res += " "
+            if mag > 3/i*18:
+                res += f" {mag:1.0f} "
+            else:
+                res += " . "
+        print(res)
 
-    print("Visualizer running. Press Ctrl+C to quit.")
+    def shutdown(self, status, reason):
+        print("JACK shutdown:", status, reason)
+
+dynamizer = Dynamizer()
+
+if __name__ == "__main__":
+    print("Dynamizer starting. Press Ctrl+C to quit.")
+    time.sleep(1)
+    dynamizer.activate()
+
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nStopping...")
 
-print("Client closed.")
+    print("Client closed, goodbye from Dynamizer")
