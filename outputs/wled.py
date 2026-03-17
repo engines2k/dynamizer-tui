f8353bd19112bd9f7015a9039ff5fd605f4723c2
engines2k <syncorex0@gmail.com>
2026-03-16 21:29:46 -0600

    support for multiple destinations in WLEDClient

--- outputs/wled.py
+++ outputs/wled.py
@@ -2,17 +2,17 @@ import socket
 import time
 from .light_buffer import LightBuffer, LightDevice
 from .delayqueue import DelayQueue
-from typing import List
+from typing import List, Tuple
 
 OUTPUT_DELAY = 0#220
-WLED_HOST = "wled-bfn.local"
+WLED_HOST = "wled-bfn2.local"
 WLED_PORT = 21324
 LISTEN_TIMEOUT_SECONDS = 2
 
 LIGHT_SETTINGS_LOW = {
     'adjust': -1000,
     'multiplier': .5,
-    'color': (1, 200, 28), #BRG
+    'color': (1, 200, 78), #BRG
     'speed': 8
 }
 
@@ -24,7 +24,7 @@ LIGHT_SETTINGS_LOW_BEAT = {
 }
 
 LIGHT_SETTINGS_HIGH = {
-    'multiplier': 1.4,
+    'multiplier': 1.1,
     'adjust': 0,
     'color': (100, 30, 15), #BRG
     'speed': 5
@@ -33,13 +33,16 @@ LIGHT_SETTINGS_HIGH = {
 class WLEDClient:
     def __init__(self):
         self.multiplier = 1.3
-        self.host = WLED_HOST
-        self.port = WLED_PORT
+        self._destinations = [
+            { 'host': 'wled-bfn.local', 'port': 21324 },
+            { 'host': 'wled-bfn2.local', 'port': 21324 }
+        ]
+        self._sockets = List[socket.SocketIO]
         self._packet_queue = DelayQueue(delay=OUTPUT_DELAY)
         self._active = False
         self._light_buffers: dict[str, LightBuffer] = {
-            'kick_harmony': LightBuffer(n_frames=90, settings=LIGHT_SETTINGS_LOW),
-            'kick_beat': LightBuffer(n_frames=90, settings=LIGHT_SETTINGS_LOW_BEAT),
+            'kick_harmony': LightBuffer(n_frames=30, settings=LIGHT_SETTINGS_LOW),
+            'kick_beat': LightBuffer(n_frames=30, settings=LIGHT_SETTINGS_LOW_BEAT),
             'snare_signal': LightBuffer(n_frames=25, settings=LIGHT_SETTINGS_HIGH),
         }
 
@@ -50,33 +53,32 @@ class WLEDClient:
                     (0, self._light_buffers['snare_signal']),
                     (26, self._light_buffers['kick_harmony']),
                     (26, self._light_buffers['kick_beat']),
-                    (120, self._light_buffers['snare_signal']),
                 ]
             ),
             LightDevice(
                 n_leds=144,
                 buffers=[
-                    (0, self._light_buffers['kick_harmony']),
-                    (0, self._light_buffers['kick_beat']),
-                    (113, self._light_buffers['snare_signal']),
+                    (0, self._light_buffers['snare_signal']),
+                    (26, self._light_buffers['kick_harmony']),
+                    (26, self._light_buffers['kick_beat']),
                 ]
             )
         ]
         self._max_send_rate_hz = 250
         self._min_send_interval = 1.0 / self._max_send_rate_hz
         self._last_send_time = 0
+        self._addresses: List[Tuple[str, str]]
 
     def activate(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._socket.setblocking(False)
-        self._resolve_address()
+        self._resolve_addresses()
         self._active = True
 
-    def _resolve_address(self):
-        try:
-            self._resolved_address = (socket.gethostbyname(WLED_HOST), WLED_PORT)
-        except socket.gaierror:
-            self._resolved_address = (WLED_HOST, WLED_PORT)
+    def _resolve_addresses(self):
+        self._addresses = []
+        for dest in self._destinations:
+            self._addresses.append((socket.gethostbyname(dest['host']), dest['port']))
 
     def _build_payload(self, features):
         payload = bytearray()
@@ -102,7 +104,8 @@ class WLEDClient:
 
         for payload in ready_payloads:
             try:
-                self._socket.sendto(payload, self._resolved_address)
+                for address in self._addresses:
+                    self._socket.sendto(payload, address)
                 self._last_send_time = time.time()
             except BlockingIOError:
                 pass
